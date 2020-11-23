d252104468552f137910607a4723a254b9f1e282
Josh Young <18503267+joshayoung@users.noreply.github.com>
2020-11-23 12:01:23 -0600

    Add a class to active record

--- programs/active_record_script.rb
+++ programs/active_record_script.rb
@@ -2,32 +2,37 @@ require "byebug"
 require "pry-byebug"
 require "active_record"
 
-ActiveRecord::Base.establish_connection(
-    adapter: "sqlite3",
-    database: "./programs/database.db"
-)
+class DatabaseSchema
+  def self.call
+    ActiveRecord::Base.establish_connection(
+      adapter: "sqlite3",
+      database: "./programs/database.db"
+    )
+    drop_tbl
+    create_tbl
+    add_to_tbl
+  end
 
-def create
-  ActiveRecord::Schema.define do
-    create_table :test_tables, force: true do |t|
-      t.string :first
-      t.string :last
-      t.datetime :year_born
+  def self.create_tbl
+    ActiveRecord::Schema.define do
+      create_table :test_tables, force: true do |t|
+        t.string :first
+        t.string :last
+        t.datetime :year_born
+      end
     end
+  end
+
+  def self.add_to_tbl
+    TestTable.find_or_create_by(id: 1)
+  end
+
+  def self.drop_tbl
+    ActiveRecord::Schema.drop_table(:test_tables, if_exists: true)
   end
 end
 
-def doit
-  res = TestTable.find_or_create_by(id: 1)
-end
-
-def drop
-  ActiveRecord::Schema.drop_table(:test_tables, if_exists: true)
-end
-
-drop
-create
 class TestTable < ActiveRecord::Base; end
-doit
+DatabaseSchema.call
 
 puts "script ended..."
